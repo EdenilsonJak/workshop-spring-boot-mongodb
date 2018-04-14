c2692ae6d886ebdab8a94b44be0de457c0ca1116
EdenilsonJak <edenilsonjak@gmail.com>
2018-04-14 15:39:32 -0300

    Atualizacao de usuario com put

--- src/main/java/com/ejsistemas/workshopmongo/services/UserService.java
+++ src/main/java/com/ejsistemas/workshopmongo/services/UserService.java
@@ -38,6 +38,17 @@ public class UserService {
 		repo.delete(id);
 	}
 	
+	public User updade(User obj){
+		User newObj = repo.findOne(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDto(UserDTO objDto){
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
